cb28aa4f78e9a8d50ca515ec7ed9dde6c5048e82
Jalil <abdullacelil00@gmail.com>
2024-11-11 19:36:28 +0400

    Text based data types

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,35 +7,11 @@ using System.Threading.Tasks;
 namespace FirstConsoleApp {
     internal class Program {
         static void Main(string[] args) {
-            //int x = 10;
-            //int y = 20;
-            //int z = 30;
-            int x = 10, y = 20, z = 30;
-
-            int age = 24;
-            Console.WriteLine(age);
-            Console.WriteLine(int.MaxValue); //2147483647
-            Console.WriteLine(int.MinValue); //-2147483648
-
-            long bigNumber = -900000000L;
-            Console.WriteLine(bigNumber);
-            Console.WriteLine(long.MaxValue); //9223372036854775807
-            Console.WriteLine(long.MinValue); //-9223372036854775808
-
-            double negative = -55.2;
-            Console.WriteLine(negative);
-            Console.WriteLine(double.MaxValue); //1,79769313486232E+308
-            Console.WriteLine(double.MinValue); //-1,79769313486232E+308
-
-            float precision = 5.000001F;
-            Console.WriteLine(precision);
-            Console.WriteLine(float.MaxValue); //3,402823E+38
-            Console.WriteLine(float.MinValue); //-3,402823E+38
-
-            decimal money = 14.99M;
-            Console.WriteLine(money);
-            Console.WriteLine(decimal.MaxValue); //79228162514264337593543950335
-            Console.WriteLine(decimal.MinValue); //-79228162514264337593543950335
+            string name = "Jalil"; //default: ""
+            char letter = 'a'; //default: \0
+            Console.Write("Your name is ");
+            Console.WriteLine(name);
+            Console.WriteLine(letter);
             Console.ReadLine();
         }
     }
