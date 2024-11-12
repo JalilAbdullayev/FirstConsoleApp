c760abb5f83b4b3808f1e70e1713c8bdcb7d3e5f
Jalil <abdullacelil00@gmail.com>
2024-11-12 06:46:33 +0400

    Boolean data type

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,25 +7,11 @@ using System.Threading.Tasks;
 namespace FirstConsoleApp {
     internal class Program {
         static void Main(string[] args) {
-            string textAge = "-24";
-            int age = Convert.ToInt32(textAge);
-            Console.WriteLine(age);
-
-            string textBigNumber = "-900000000";
-            long bigNumber = Convert.ToInt64(textBigNumber);
-            Console.WriteLine(bigNumber);
-
-            string textNegative = "-55.2";
-            double negative = Convert.ToDouble(textNegative);
-            Console.WriteLine(negative);
-
-            string textPrecision = "5.000001";
-            float precision = Convert.ToSingle(textPrecision);
-            Console.WriteLine(precision);
-
-            string textMoney = "14.99";
-            decimal money = Convert.ToDecimal(textMoney);
-            Console.WriteLine(money);
+            bool value = true;
+            bool isMale = true;
+            Console.WriteLine(isMale);
+            isMale = false;
+            Console.WriteLine(isMale);
             Console.ReadLine();
         }
     }
