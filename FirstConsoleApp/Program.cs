d3fa6e9d7bfeeae127bef510c4b346512a5f597c
Jalil <abdullacelil00@gmail.com>
2024-11-12 07:03:48 +0400

    Operators

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,11 +7,27 @@ using System.Threading.Tasks;
 namespace FirstConsoleApp {
     internal class Program {
         static void Main(string[] args) {
-            bool value = true;
-            bool isMale = true;
-            Console.WriteLine(isMale);
-            isMale = false;
-            Console.WriteLine(isMale);
+            double age = 24;
+            //age++; //25
+            //age--; //24
+            //age = age + 10;
+            //age += 10 // 34;
+            //age -= 10 // 14;
+            //age *= 10 // 240;
+            age /= 10; // int-dirsə 2.4 -> 2. Double-da elə 2.4.
+            Console.WriteLine(age);
+
+            string name = "Jalil";
+            name += " is a programmer.";
+            Console.WriteLine(name); // Jalil is a programmer.
+
+            char ch = 'a';
+            ch += 'b';
+            Console.WriteLine(ch);
+
+            int i = 0;
+            Console.WriteLine(i++); // 0
+            Console.WriteLine(++i); // 2
             Console.ReadLine();
         }
     }
