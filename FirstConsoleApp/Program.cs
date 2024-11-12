bce4a279789dc3223e8afdb5c468a84ea9d32f51
Jalil <abdullacelil00@gmail.com>
2024-11-12 06:40:57 +0400

    Convert strings to numbers

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 namespace FirstConsoleApp {
     internal class Program {
         static void Main(string[] args) {
-            string name = "Jalil"; //default: ""
-            char letter = 'a'; //default: \0
-            Console.Write("Your name is ");
-            Console.WriteLine(name);
-            Console.WriteLine(letter);
+            string textAge = "-24";
+            int age = Convert.ToInt32(textAge);
+            Console.WriteLine(age);
+
+            string textBigNumber = "-900000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
+
+            string textNegative = "-55.2";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+
+            string textPrecision = "5.000001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
+
+            string textMoney = "14.99";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
             Console.ReadLine();
         }
     }
